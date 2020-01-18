b6d4eb969bb6da6bd6bfefda988eed78a5f3ec04
Ivan <ivan-kerch@mail.ru>
2020-01-18 21:46:26 +0200

    the forms block is finished

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist = "/Applications/MAMP/htdocs/test"; // Тут адрес к вашему серверу
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
